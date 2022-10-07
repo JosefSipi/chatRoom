5a946dd1f3f799bca193e2813176203e267935fe
Josef <josephsipiorski@gmail.com>
2022-10-07 10:47:08 -0500

    add to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+const socket = new WebSocket("wss://tso-take-home-chat-room.herokuapp.com");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [userState, setUserState] = useState(() => {
+    return {};
+  });
+
+  socket.onmessage = ({ data }) => {
+    const [user, message] = data.split(": ");
+    const messageCount = message.split(" ").length;
+
+    if (userState[user] !== undefined) {
+      setUserState((prevState) => {
+        let newState = { ...prevState };
+        newState[user] = prevState[user] + messageCount;
+        return newState;
+      });
+    } else {
+      setUserState((prevState) => {
+        let newState = { ...prevState };
+        newState[user] = messageCount;
+        return newState;
+      });
+    }
+  };
+
+  const userComponent = (name, count) => {
+    return (
+      <div key={`${name}`} className="user">
+        <h3>name: {name} </h3>
+        <h3>Total count: {count}</h3>
+      </div>
+    );
+  };
+
+  const list = () => {
+    let list = [];
+
+    for (const key in userState) {
+      list.push([key, userState[key]]);
+    }
+    list.sort((a,b) => b[1] - a[1])
+
+    return list.map((use) => {
+      return userComponent(use[0], use[1]);
+    });
+  };
+
+  return <div className="App">{!!Object.keys(userState)[0] && list()}</div>;
 }
 
 export default App;
